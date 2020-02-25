818318813e3e230c66f83eb36f5a89a1869ffbb2
Mark Holcomb <markholcomb5@gmail.com>
2020-02-25 17:56:30 -0500

    Updated documentation to include explanation

--- Python/Greedy/active-select.py
+++ Python/Greedy/active-select.py
@@ -5,18 +5,22 @@
 startTime = [1,3,0,5,8,5]
 stopTime =  [2,4,6,7,9,9]
 
+## Active select starts here
 # 2. Take the job that finishes the soonest relative to the current time
 def act_sel(startList, finishList):
     accepted = 0
     i = 0
-    while i < len(finishList): # stop chosen arbitrarily as end, start would work too
+    while i < len(finishList): # stop condition chosen arbitrarily as the stopTime length, start would work too
         if startList[i] >= accepted: # if the current start value is of equal or greater value than the last accepted job's end...
             print(i) 
             accepted = finishList[i] # store the current job's end in accepted
         i = i + 1
 
-# This algorithm is greedy because it looks at the first (closest) end time and starts the algorithm from there. 
+## Active select ends here
 # From that spot it continues taking the next available job.
+# This does the job in O(n) time, because you list through your input once. 
+# This gives a total of O(N Log N) because we are bound by the sorting below.
+
 
 # Now, what if we are given two unsorted lists?
 # Let's sort them ourselves!
